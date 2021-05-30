1008bae4c45432b743f17c850fb464127db9b382
Iliya <iliyamiroshnichenko88@gmail.com>
2021-05-30 18:01:10 +0300

    prepare contacts.js

---
+++ contacts.js
@@ -0,0 +1,25 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
+const fs = require("fs");
+const path = require("path");
+
+const contactsPath = "";
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
